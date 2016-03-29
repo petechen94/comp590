3f2bad5415e55f66109c378ca329146ec31c86cd
Danila Chenchik <chenchik@email.unc.edu>
2016-03-28 23:48:40 -0400

    added main directory

---
+++ HelprApp/app/src/main/java/helperapp/chenchik/helprapp/NewListingActivity.java
@@ -0,0 +1,47 @@
+package helperapp.chenchik.helprapp;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+
+/**
+ * Created by chenchik on 3/28/2016.
+ */
+public class NewListingActivity extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_listing);
+        //x = 0;
+        //y = 1000;
+
+
+
+    }
+
+
+}
